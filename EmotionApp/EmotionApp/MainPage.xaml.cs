44394b806ba0c27f04fb7e8ff5efb09c08171514
Bryan Anthony Garcia <bryan@xam-consulting.com>
2019-06-04 21:15:15 +1000

    Added cognitive service

--- EmotionApp/EmotionApp/MainPage.xaml.cs
+++ EmotionApp/EmotionApp/MainPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using Plugin.Media;
+﻿using Microsoft.Azure.CognitiveServices.Vision.Face;
+using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
+using Plugin.Media;
 using Plugin.Media.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,42 +13,11 @@ namespace EmotionApp
 {
     public partial class MainPage : ContentPage
     {
+        private Stream stream;
+
         public MainPage()
         {
             InitializeComponent();
-
-            //takePhoto.Clicked += async (sender, args) =>
-            //{
-
-            //    if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
-            //    {
-            //        DisplayAlert("No Camera", ":( No camera available.", "OK");
-            //        return;
-            //    }
-
-            //    var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
-            //    {
-            //        Directory = "Test",
-            //        SaveToAlbum = true,
-            //        CompressionQuality = 75,
-            //        CustomPhotoSize = 50,
-            //        PhotoSize = PhotoSize.MaxWidthHeight,
-            //        MaxWidthHeight = 2000,
-            //        DefaultCamera = CameraDevice.Front
-            //    });
-
-            //    if (file == null)
-            //        return;
-
-            //    DisplayAlert("File Location", file.Path, "OK");
-
-            //    image.Source = ImageSource.FromStream(() =>
-            //    {
-            //        var stream = file.GetStream();
-            //        file.Dispose();
-            //        return stream;
-            //    });
-            //};
 
             pickPhoto.Clicked += async (sender, args) =>
             {
@@ -64,9 +36,15 @@ namespace EmotionApp
                 if (file == null)
                     return;
 
+                activityIndicator.IsVisible = true;
+                stream = file.GetStream();
+                await IdentifyEmotion(stream);
+
+                activityIndicator.IsVisible = false;
+
                 image.Source = ImageSource.FromStream(() =>
                 {
-                    var stream = file.GetStream();
+                    stream = file.GetStream();
                     file.Dispose();
                     return stream;
                 });
@@ -95,14 +73,54 @@ namespace EmotionApp
             if (file == null)
                 return;
 
+            activityIndicator.IsVisible = true;
+            stream = file.GetStream();
+            await IdentifyEmotion(stream);
+            activityIndicator.IsVisible = false;
             image.Source = ImageSource.FromStream(() =>
             {
-                var stream = file.GetStream();
+                stream = file.GetStream();
                 file.Dispose();
                 return stream;
             });
-
-            resultText.Text = "You look like you're\n SAD";
         }
+
+        private async Task IdentifyEmotion(Stream stream)
+        {
+            FaceClient faceClient = new FaceClient(
+       new ApiKeyServiceClientCredentials("0ccd2300a2b54e8caed6562aaedaca1e"),
+       new System.Net.Http.DelegatingHandler[] { });
+            faceClient.Endpoint = "https://southeastasia.api.cognitive.microsoft.com/";
+
+            IList<DetectedFace> faceList =
+                    await faceClient.Face.DetectWithStreamAsync(
+                        stream, true, false, new FaceAttributeType[] { FaceAttributeType.Emotion, FaceAttributeType.Age });
+            if (faceList != null && faceList.Count > 0)
+            {
+                string result = "";
+                if (faceList[0].FaceAttributes.Emotion.Happiness > 0.70)
+                    result = "HAPPY :)";
+                else if (faceList[0].FaceAttributes.Emotion.Sadness > 0.70)
+                    result = "SAD :(";
+                else if (faceList[0].FaceAttributes.Emotion.Fear > 0.70)
+                    result = "SCARED :s";
+                else if (faceList[0].FaceAttributes.Emotion.Anger > 0.70)
+                    result = "ANGRY >:(";
+                else if (faceList[0].FaceAttributes.Emotion.Surprise > 0.70)
+                    result = "SURPRISED :O";
+                else if (faceList[0].FaceAttributes.Emotion.Disgust > 0.70)
+                    result = "DISGUSTED xO";
+                else if (faceList[0].FaceAttributes.Emotion.Contempt > 0.70)
+                    result = "CONTEMPT :/";
+                else
+                    result = "EMOTIONLESS";
+
+                resultText.Text = $"You look like you're\n {result}";
+            }
+            else
+                resultText.Text = $"No face found";
+
+        }
+
     }
 }
